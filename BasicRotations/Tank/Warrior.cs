eb9dde694f4017f5edaefabb263c504b873a0013
spaceelysian <148630011+spaceelysian@users.noreply.github.com>
2024-04-14 05:52:51 -0700

    Add files via upload

--- BasicRotations/Tank/Warrior.cs
+++ BasicRotations/Tank/Warrior.cs
@@ -6,23 +6,41 @@ public sealed class WARPvP : WarriorRotation
     {
 
         return base.EmergencyAbility(nextGCD, out act);
+
     }
 
     protected override bool AttackAbility(IAction nextGCD, out IAction? act)
     {
+        // if (BlotaPvP.CanUse(out act)) return true;
+
+        if (OrogenyPvP.CanUse(out act)) return true;
         if (OnslaughtPvP.CanUse(out act)) return true;
 
         return base.AttackAbility(nextGCD, out act);
+
+    }
+    protected override bool GeneralAbility(IAction nextGCD, out IAction? act)
+    {
+
+        if (BloodwhettingPvP.CanUse(out act)) return true;
+
+        return base.GeneralAbility(nextGCD, out act);
+
     }
     protected override bool GeneralGCD(out IAction? act)
     {
 
         if (PrimalRendPvP.CanUse(out act)) return true;
 
+        if (ChaoticCyclonePvP.CanUse(out act)) return true;
+
+        // if (FellCleavePvP.CanUse(out act)) return true;
+
         if (StormsPathPvP.CanUse(out act)) return true;
         if (MaimPvP.CanUse(out act)) return true;
         if (HeavySwingPvP.CanUse(out act)) return true;
 
         return base.GeneralGCD(out act);
+
     }
 }
