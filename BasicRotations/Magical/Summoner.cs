5e4af9454b8c97e2ddc3251aa6b28d6f1dcea59c
spaceelysian <148630011+spaceelysian@users.noreply.github.com>
2024-04-14 06:42:16 -0700

    Add files via upload

--- BasicRotations/Magical/Summoner.cs
+++ BasicRotations/Magical/Summoner.cs
@@ -1,10 +1,11 @@
 ﻿namespace DefaultRotations.Magical;
-
 [Rotation("smn-pvp", CombatType.PvP, GameVersion = "6.58", Description = "pvp skills")]
 public class SMNPvP : SummonerRotation
 {
     protected override bool EmergencyAbility(IAction nextGCD, out IAction? act)
     {
+
+        if (RadiantAegisPvP.CanUse(out act)) return true;
 
         return base.EmergencyAbility(nextGCD, out act);
     }
@@ -12,11 +13,27 @@ public class SMNPvP : SummonerRotation
     protected override bool AttackAbility(IAction nextGCD, out IAction? act)
     {
 
+        if (FesterPvP.CanUse(out act)) return true;
+        if (MountainBusterPvP.CanUse(out act)) return true;
+        if (EnkindleBahamutPvP.CanUse(out act)) return true;
+        if (EnkindlePhoenixPvP.CanUse(out act)) return true;
+
         return base.AttackAbility(nextGCD, out act);
     }
+    protected override bool GeneralAbility(IAction nextGCD, out IAction? act)
+    {
 
+        return base.GeneralAbility(nextGCD, out act);
+    }
     protected override bool GeneralGCD(out IAction? act)
     {
+
+        if (SlipstreamPvP.CanUse(out act)) return true;
+
+        if (RuinIiiPvP.CanUse(out act)) return true;
+
+        if (CrimsonStrikePvP.CanUse(out act)) return true;
+        if (CrimsonCyclonePvP.CanUse(out act)) return true;
 
         return base.GeneralGCD(out act);
     }
