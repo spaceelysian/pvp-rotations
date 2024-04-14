cc84524bfe2ec83c772c263b45e607441e197494
spaceelysian <148630011+spaceelysian@users.noreply.github.com>
2024-04-14 07:29:19 -0700

    Add files via upload

--- BasicRotations/Healer/Sage.cs
+++ BasicRotations/Healer/Sage.cs
@@ -1,5 +1,4 @@
 namespace DefaultRotations.Healer;
-
 [Rotation("sge-pvp", CombatType.PvP, GameVersion = "6.58", Description = "pvp skills")]
 public class SGEPvP : SageRotation
 {
@@ -7,17 +6,33 @@ public class SGEPvP : SageRotation
     {
 
         return base.EmergencyAbility(nextGCD, out act);
+
     }
 
     protected override bool AttackAbility(IAction nextGCD, out IAction? act)
     {
 
-        return base.AttackAbility(nextGCD, out act);
-    }
+        if (ToxikonPvP.CanUse(out act)) return true;
 
+        return base.AttackAbility(nextGCD, out act);
+
+    }
+    protected override bool GeneralAbility(IAction nextGCD, out IAction? act)
+    {
+
+        return base.GeneralAbility(nextGCD, out act);
+
+    }
     protected override bool GeneralGCD(out IAction? act)
     {
 
+        if (PneumaPvP.CanUse(out act)) return true;
+
+        if (PhlegmaIiiPvP.CanUse(out act)) return true;
+
+        if (DosisIiiPvP.CanUse(out act)) return true;
+
         return base.GeneralGCD(out act);
+
     }
 }
