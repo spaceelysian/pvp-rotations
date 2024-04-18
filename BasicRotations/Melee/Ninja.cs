e73a3715e1d7942b2e59b4b37b6c02a53ec73fb4
spaceelysian <148630011+spaceelysian@users.noreply.github.com>
2024-04-18 10:41:33 -0700

    Update Ninja.cs

--- BasicRotations/Melee/Ninja.cs
+++ BasicRotations/Melee/Ninja.cs
@@ -13,21 +13,40 @@ public class NINPvP : NinjaRotation
     protected override bool AttackAbility(IAction nextGCD, out IAction? act)
     {
 
+        if (!Player.HasStatus(true, StatusID.ThreeMudra) && FumaShurikenPvP.Cooldown.CurrentCharges <= 1 && MugPvP.CanUse(out act)) return true;
+
         return base.AttackAbility(nextGCD, out act);
     }
     protected override bool GeneralAbility(IAction nextGCD, out IAction? act)
     {
+
 
         return base.GeneralAbility(nextGCD, out act);
     }
     protected override bool GeneralGCD(out IAction? act)
     {
 
+        if (Player.HasStatus(true, StatusID.Hidden_1705))
+        {
+            act = null;
+            if (AssassinatePvP.CanUse(out act)) return true;
+            return false;
+        }
+
+        if (Player.HasStatus(true, StatusID.ThreeMudra))
+        {
+            act = null;
+            return false;
+        }
+
+        if (FumaShurikenPvP.Cooldown.CurrentCharges == 3 && FumaShurikenPvP.CanUse(out act)) return true;
+
         if (AeolianEdgePvP.CanUse(out act)) return true;
         if (GustSlashPvP.CanUse(out act)) return true;
         if (SpinningEdgePvP.CanUse(out act)) return true;
 
-        if (FumaShurikenPvP.CanUse(out act)) return true;
+        if (FumaShurikenPvP.Cooldown.CurrentCharges > 1 && FumaShurikenPvP.CanUse(out act, usedUp: true)) return true;
+        if (FumaShurikenPvP.Target.Target?.GetHealthRatio() < 0.5 && FumaShurikenPvP.CanUse(out act, usedUp: true)) return true;
 
         return base.GeneralGCD(out act);
     }
